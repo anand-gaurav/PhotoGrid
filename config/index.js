a7a8939a9a26577cc218b51aeb9b49a17fc57370
anand-gaurav <42348633+anand-gaurav@users.noreply.github.com>
2018-08-27 22:38:53 +0530

    remove cred

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@
 if(process.env.NODE_ENV === 'production'){    
     module.exports = {
         host: process.env.host || "",
-        S3AccessKey: "AKIAITFZZKIXCRSCVONQ",
-        S3Secret : "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",
-        S3Bucket: "photogreedy"
+        S3AccessKey: "",
+        S3Secret : "",
+        S3Bucket: ""
     }
 }else{
     //Offer dev stage settings and data
